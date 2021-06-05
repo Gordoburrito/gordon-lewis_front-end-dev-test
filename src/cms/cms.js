7c13d60e23eb9b579b5ce2d4cff4261e0330d2c7
Gordon <glewis@berkeley.edu>
2021-06-04 19:34:38 -0700

    Edit to youtube id and add preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -15,25 +15,56 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerEditorComponent({
+  // Internal id of the component
   id: "youtube",
-  label: "YouTube",
-  fields: [
-    {
-      name: "url",
-      label: "Youtube video URL",
-      widget: "string",
-    },
-  ],
-  pattern: /^`youtube:\s(.*)`$/,
-  fromBlock: function (match) {
+  // Visible label
+  label: "Youtube",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^youtube (\S+)$/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function(match) {
     return {
-      url: match[1],
-    }
+      id: match[1]
+    };
   },
-  toBlock: function (obj) {
-    return "`youtube: " + obj.url + "`"
+  // Function to create a text block from an instance of this component
+  toBlock: function(obj) {
+    return 'youtube ' + obj.id;
   },
-  toPreview: function (obj) {
-    return obj.url
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+    );
+  }
+});
+CMS.registerEditorComponent({
+  // Internal id of the component
+  id: "youtube",
+  // Visible label
+  label: "Youtube",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^youtube (\S+)$/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
   },
-})
+  // Function to create a text block from an instance of this component
+  toBlock: function(obj) {
+    return 'youtube ' + obj.id;
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+    );
+  }
+});
